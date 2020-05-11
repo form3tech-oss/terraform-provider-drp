0caebc8ed8162649095d899693546b91e1aa79d8
Miklos Barabas <miklos.barabas@form3.tech>
2020-05-11 20:09:51 +0200

    fixing package.sh - junk-paths for zips - version the binary

--- tools/package.sh
+++ tools/package.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-VERSION=$(echo $TRAVIS_TAG | sed 's/v//g')
+VERSION=$(echo ${TRAVIS_TAG} | sed 's/v//g')
 
 BINARY="bin/darwin/amd64/terraform-provider-drp"
-zip "${BINARY}_${VERSION}_darwin_amd64.zip" "${BINARY}"
+BINARY_TAGGED=${BINARY}_${TRAVIS_TAG}
+mv ${BINARY} ${BINARY_TAGGED}
+zip -j "${BINARY}_${VERSION}_darwin_amd64.zip" "${BINARY_TAGGED}"
 
 BINARY="bin/linux/amd64/terraform-provider-drp"
-zip "${BINARY}_${VERSION}_linux_amd64.zip" "${BINARY}"
+BINARY_TAGGED=${BINARY}_${TRAVIS_TAG}
+mv ${BINARY} ${BINARY_TAGGED}
+zip -j "${BINARY}_${VERSION}_linux_amd64.zip" "${BINARY_TAGGED}"
