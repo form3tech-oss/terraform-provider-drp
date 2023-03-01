5d9d199698cdbcf48bfc5d2b664e8d82a06b7a51
Fábio Matavelli <fabiomatavelli@gmail.com>
2023-03-01 18:49:51 +0000

    feat: add reservation resource

--- drpv4/provider.go
+++ drpv4/provider.go
@@ -22,13 +22,14 @@ func Provider() *schema.Provider {
 	return &schema.Provider{
 
 		ResourcesMap: map[string]*schema.Resource{
-			"drp_machine":  resourceMachine(),
-			"drp_param":    resourceParam(),
-			"drp_template": resourceTemplate(),
-			"drp_task":     resourceTask(),
-			"drp_stage":    resourceStage(),
-			"drp_workflow": resourceWorkflow(),
-			"drp_subnet":   resourceSubnet(),
+			"drp_machine":     resourceMachine(),
+			"drp_param":       resourceParam(),
+			"drp_template":    resourceTemplate(),
+			"drp_task":        resourceTask(),
+			"drp_stage":       resourceStage(),
+			"drp_workflow":    resourceWorkflow(),
+			"drp_subnet":      resourceSubnet(),
+			"drp_reservation": resourceReservation(),
 		},
 
 		// note yet, but potentially pools, params and profiles
